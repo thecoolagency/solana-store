01b765c93b6109e2b5aaed68b6fcc71b7737eb02
Jonah Madeya <jonah@thecoolagency.com>
2022-06-30 09:29:01 -0700

    update

--- components/Head.js
+++ components/Head.js
@@ -7,23 +7,23 @@ export default function HeadComponent() {
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta name="theme-color" content="#000000" />
 
-      <title>Solana Pay Store</title>
-      <meta name="title" content="Solana Pay Store" />
-      <meta name="description" content="Buy items on my store using Solana Pay!" />
+      <title>TCA Storefront w/ Solana</title>
+      <meta name="title" content="TCA Storefront" />
+      <meta name="description" content="Buy items on my store using Solana Pay" />
 
       {/* Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://buildspace.so/" />
-      <meta property="og:title" content="Solana Pay Store" />
+      <meta property="og:url" content="https://jonahlmadeya.com/" />
+      <meta property="og:title" content="TCA Storefront" />
       <meta property="og:description" content="Buy items on my store using Solana Pay!" />
-      <meta property="og:image" content="https://cdn.buildspace.so/courses/solana-pay/metadata.png" />
+      <meta property="og:image" content="https://www.jonahlmadeya.com/files/yes.GIF" />
 
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://buildspace.so/" />
-      <meta property="twitter:title" content="Solana Pay Store" />
+      <meta property="twitter:url" content="https://jonahlmadeya.com/" />
+      <meta property="twitter:title" content="TCA Storefront" />
       <meta property="twitter:description" content="Buy items on my store using Solana Pay!" />
-      <meta property="twitter:image" content="https://cdn.buildspace.so/courses/solana-pay/metadata.png" />
+      <meta property="twitter:image" content="https://www.jonahlmadeya.com/files/yes.GIF" />
     </Head>
   );
 }
